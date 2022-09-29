295b9d71dd41aeffef6eec120dd4d0d2a5c06f80
Tao Bian <biantao0519@hotmail.com>
2022-09-29 10:27:14 +1000

    working on client

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -32,24 +32,20 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const { data } = await createUser({
+        variables: { ...userFormData },
+      });
+      
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
     }
 
     setUserFormData({
-      username: '',
-      email: '',
-      password: '',
+      username: "",
+      email: "",
+      password: "",
     });
   };
 
